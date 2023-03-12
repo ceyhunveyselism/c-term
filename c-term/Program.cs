904cbda14c8d2a82af495cac1c82dd4785dafa1b
ceyhunveysel <ceyhun.veysel35@gmail.com>
2023-03-12 23:48:44 +0300

    Added main start line. Will implement lexer next.
    Main start line has a gimmick with the username and the amount of times that the user has launched c-term. I'm not sure why I implemented this, or why it took so long for me to do so, but things happen, and in the name of the man Deltact himself, when you can do a cool gimmick, you should do that cool gimmick. Did it cost me 2 functions and some valuable memory? Yes. Do I care? Nah, not really.

--- c-term/Program.cs
+++ c-term/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace c_term
 {
@@ -10,6 +12,34 @@ namespace c_term
     {
         static void Main(string[] args)
         {
+            string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            usesUpdate();
+            Console.WriteLine($"Hello, {userName}! This is your ${getUses()} time using c-term.");
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("b1-v0.0.1 | Type help for commands");
+        }
+
+
+        static string usesUpdate()
+        {
+            string ctfile = Path.GetTempPath() + "cterm.ct";
+            if(File.Exists(ctfile))
+            {
+                File.WriteAllText(ctfile, (Int32.Parse(File.ReadAllText(ctfile)) + 1).ToString());
+                return File.ReadAllText(ctfile);
+            } else
+            {
+                File.Create(ctfile);
+                File.WriteAllText(ctfile, "1");
+                return "1";
+            }
+        }
+
+        static string getUses()
+        {
+            string ctfile = Path.GetTempPath() + "cterm.ct";
+            string text = File.ReadAllText(ctfile);
+            return text == "1" ? "1st" : text == "2" ? "2nd" : text == "3" ? "3rd" : text + "st"; 
         }
     }
 }
