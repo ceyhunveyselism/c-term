fa11ad0d4d7a12cb793b7e84f8cf38587eefaa26
ceyhunveysel <ceyhun.veysel35@gmail.com>
2023-03-14 15:47:09 +0300

    Fix build name and version
    I forgor :skull:

--- c-term/Program.cs
+++ c-term/Program.cs
@@ -18,7 +18,7 @@ namespace c_term
             usesUpdate();
             Console.WriteLine($"Hello, {userName.Split('\\')[1]}! This is your {getUses()} time using c-term.");
             Console.WriteLine("--------------------------------------------------");
-            Console.WriteLine("b12-v1.5.5 | Type help for commands\n");
+            Console.WriteLine("b13-v1.5.6 | Type help for commands\n");
 
             List<Command> commandList = new List<Command>();
             commandList.Add(new Ping());
