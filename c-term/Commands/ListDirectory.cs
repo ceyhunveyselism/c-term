2973ffcc102c5ead773afd64d329a98ae5bcc8a4
ceyhunveysel <ceyhun.veysel35@gmail.com>
2023-03-14 16:26:50 +0300

    Adds argument to not use full path in listdir

--- c-term/Commands/ListDirectory.cs
+++ c-term/Commands/ListDirectory.cs
@@ -12,13 +12,29 @@ namespace c_term.Commands
         public ListDirectory()
         {
             description = "Lists every file in the current directory.";
-            usage = "listdir";
+            usage = "listdir [don't use full path?]";
             aliases = new string[] { "listdir", "listdirectory", "dir" };
         }
 
         public override CommandReply run(List<string> arguments)
         {
-            return new CommandReply(false, _QF.join(Directory.GetFiles(Directory.GetCurrentDirectory()).ToList<string>(), "\n"));
+            string pathing = _QF.join(Directory.GetFiles(Directory.GetCurrentDirectory()).ToList<string>(), "\n");
+            if(arguments.Count > 0)
+            {
+                List<string> newPathing = new List<string>();
+                List<string> newerPathing = new List<string>();
+                newPathing = Directory.GetFiles(Directory.GetCurrentDirectory()).ToList<string>();
+
+                for(int i = 0; i < newPathing.Count; i++)
+                {
+                    List<string> newernewerPathing = new List<string>(newPathing[i].Split('\\').ToList<string>()); // GOD IM FUCKING DONE WITH MAKING LISTS FOR FUCKS SAKE THIS IS THE 3RD FUCKING TIME
+                    newerPathing.Add(newernewerPathing[newernewerPathing.Count - 1]);
+                }
+
+                return new CommandReply(false, _QF.join(newerPathing, "\n"));
+            }
+
+            return new CommandReply(false, pathing);
         }
     }
 }
--- c-term/Program.cs
+++ c-term/Program.cs
@@ -18,7 +18,7 @@ namespace c_term
             usesUpdate();
             Console.WriteLine($"Hello, {userName.Split('\\')[1]}! This is your {getUses()} time using c-term.");
             Console.WriteLine("--------------------------------------------------");
-            Console.WriteLine("b14-v1.5.7 | Type help for commands\n");
+            Console.WriteLine("b15-v1.5.8 | Type help for commands\n");
 
             List<Command> commandList = new List<Command>();
             commandList.Add(new Ping());
